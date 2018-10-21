da9ecc8885a7df7a2beec114a97eb0f0191544a4
Fabricio Lemos <fabricio.lemos@gmail.com>
2018-10-21 18:32:39 -0400

    refactor input listener

--- src/main/java/com/eco/rating/adapter/StandardInputListener.java
+++ src/main/java/com/eco/rating/adapter/StandardInputListener.java
@@ -5,7 +5,10 @@ import com.eco.rating.application.RatingApplicationService;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.function.BiConsumer;
 import java.util.regex.Pattern;
+
+import static java.lang.Double.parseDouble;
 
 public class StandardInputListener {
 
@@ -13,7 +16,7 @@ public class StandardInputListener {
     private static final Pattern ELEMENT_SEPARATOR_PATTERN = Pattern.compile("\" ");
 
     private final PrintStream output;
-    private Scanner scanner ;
+    private Scanner scanner;
     private RatingApplicationService ratingApplicationService;
 
     public StandardInputListener(InputStream inputStream, PrintStream outputStream, RatingApplicationService ratingApplicationService) {
@@ -23,42 +26,58 @@ public class StandardInputListener {
     }
 
     public void processDataInput() {
+        BiConsumer<String[], String> dataInputProcessor = (inputLineEntries, userName) -> {
+            double rValue = parseDouble(inputLineEntries[2]);
+
+            String[] location = inputLineEntries[1].split("/");
+
+            String countryName = this.trimQuotes(location[0]);
+            String stateName = this.trimQuotes(location[1]);
+            String cityName = this.trimQuotes(location[2]);
+
+            ratingApplicationService.processNewUser(userName, rValue, countryName, stateName, cityName);
+        };
+
+        this.processInput(dataInputProcessor);
+    }
+
+    public void processQueryInput() {
+        BiConsumer<String[], String> queryInputProcessor = (inputLineEntries, userName) -> {
+            String regionString = inputLineEntries[1];
+            String[] regionArray = regionString.split("/");
+            String countryName = trimQuotes(regionArray[0]);
+            String stateName = getStateName(regionArray);
+            String cityName = getCityName(regionArray);
+
+            int rating = StandardInputListener.this.ratingApplicationService.getRating(userName, countryName, stateName, cityName);
+
+            StringBuilder queryOutput = new StringBuilder("\"").append(userName).append("\" ").append(regionString).append(' ').append(rating);
+            output.println(queryOutput);
+        };
+
+        this.processInput(queryInputProcessor);
+    }
+
+    private void processInput(BiConsumer<String[], String> inputLineProcessOperation) {
         String line = scanner.nextLine().trim();
 
         while (!line.isEmpty()) {
-            String[] strings = ELEMENT_SEPARATOR_PATTERN.split(line);
 
-            String[] locationInputArray = strings[1].split("/");
-            String countryName = this.trimQuotes(locationInputArray[0]);
-            String stateName = this.trimQuotes(locationInputArray[1]);
-            String cityName = this.trimQuotes(locationInputArray[2]);
-            double rValue = Double.parseDouble(strings[2]);
-            String userName = strings[0].substring(1);
+            String[] inputLineEntries = ELEMENT_SEPARATOR_PATTERN.split(line);
+            String userName = trimQuotes(inputLineEntries[0]);
 
-            ratingApplicationService.processNewUser(userName, rValue, countryName, stateName, cityName);
+            inputLineProcessOperation.accept(inputLineEntries, userName);
 
             line = scanner.nextLine().trim();
         }
     }
 
-    public void processQueryInput() {
-        String line = scanner.nextLine().trim();
+    private String getCityName(String[] regionArray) {
+        return regionArray.length >= 3 ? trimQuotes(regionArray[2]) : null;
+    }
 
-        while (!line.isEmpty()) {
-            String[] strings = ELEMENT_SEPARATOR_PATTERN.split(line);
-            String userName = strings[0].substring(1);
-            String regionString = strings[1];
-            String[] regionArray = regionString.split("/");
-            String countryName = trimQuotes(regionArray[0]);
-            String stateName = regionArray.length >= 2 ? trimQuotes(regionArray[1]) : null;
-            String cityName = regionArray.length >= 3 ? trimQuotes(regionArray[2]) : null;
-
-            int rating = this.ratingApplicationService.getRating(userName, countryName, stateName, cityName);
-
-            output.println(new StringBuilder("\"").append(userName).append("\" ").append(regionString).append(' ').append(rating));
-
-            line = scanner.nextLine().trim();
-        }
+    private String getStateName(String[] regionArray) {
+        return regionArray.length >= 2 ? trimQuotes(regionArray[1]) : null;
     }
 
     private String trimQuotes(CharSequence s) {
